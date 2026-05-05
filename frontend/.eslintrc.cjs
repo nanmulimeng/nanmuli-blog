69f45f4d0cdf2d712e18396d8dc913e360898028
nanmulimeng <1679969808@qq.com>
2026-05-05 18:00:36 +0800

    chore(frontend): 新增ESLint配置文件

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,37 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    // Vue
+    'vue/multi-word-component-names': 'off',
+    'vue/no-mutating-props': 'error',
+    'vue/require-default-prop': 'off',
+    'vue/require-prop-types': 'off',
+
+    // TypeScript
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+
+    // General
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-undef': 'off', // TypeScript handles this
+  },
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly',
+  },
+}
